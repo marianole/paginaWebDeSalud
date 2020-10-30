09619176390fcabb22f4bac31c4064cd4b132ec1
mariano ledesma <marianoledesma2001@gmail.com>
2020-10-30 20:24:56 -0300

    hice las primeras validaciones del nombre y apellido y el email.Es una única function con varias lineas de codigo adentro. Al final hay que ponerle el submit para que funcione

--- js/contacto.js
+++ js/contacto.js
@@ -1,1 +1,21 @@
 /*Hello World*/
+function validarFormulario(){
+    var regexp = /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
+    if(document.formularioCompleto.nom_Apellido.lenght==0){
+
+        alert("Debe completar nombre y apellido")
+        document.formularioCompleto.nom_Apellido.focus()
+        return 0;
+    }
+    if ((regexp.test(document.formularioCompleto.mail.value) == 0) || (document.formularioCompleto.mail.value.length = 0)) {
+        alert("Introduzca una direccion de email valida");
+        document.formularioCompleto.mail.focus();
+        return 0;
+    }
+    else {
+        var c_email=true;
+    }
+
+
+
+}
