10ae9cc3bcc27f7f605f6291a0f237b3806ab008
cristian <cquipildor4@gmail.com>
2020-11-03 11:06:05 -0300

    javascript de detalle de enfermedades

---
+++ js/formulario_detalleDeEnfermedades.js
@@ -0,0 +1,129 @@
+
+
+
+function validacion(){
+  var mensajeDeError="";
+  var divContenedor=document.getElementById('contenedor');
+  var error=true;
+
+  var nombreYapellido=document.getElementById('NombreYapellido').value;
+  if(!nombreYapellido){
+    mensajeDeError+="<p>Falta completar el campo nombre y apellido</p>";
+    error=false;
+  }
+
+  var dni=document.getElementById('documento').value;
+  if(!dni){
+    mensajeDeError+="<p>Falta completar el campo dni</p>";
+    error=false;
+  }
+
+  var telefono=document.getElementById('telefono').value;
+  if(!telefono){
+    mensajeDeError+="<p>Falta completar el campo telefono</p>";
+    error=false;
+  }
+
+  var sexo=document.getElementsByName('sexo');
+  var prueba=false;
+
+  for(i in sexo){
+    if(sexo[i].checked){
+        prueba=true;
+        }
+  }
+
+   if(prueba==false){
+     mensajeDeError+="<p>Elija un opcion en el campo sexo</p>";
+     error=false;
+   }
+
+   var prueba2=false;
+   var viajes=document.getElementsByName('viajes');
+
+   for(var i in viajes){
+     if(viajes[i].checked){
+       prueba2=true;
+     }
+   }
+
+   if(prueba2==false){
+     mensajeDeError+="<p>Responda si a viajado</p>";
+     error=false;
+   }
+
+   var prueba3=false;
+   var fiebre=document.getElementsByName('fiebre');
+
+   for(var i in fiebre){
+     if(fiebre[i].checked){
+       prueba3=true;
+     }
+   }
+
+   if(prueba3==false){
+     mensajeDeError+="<p>Responda si tuvo fiebre fiebre</p>";
+     error=false;
+   }
+
+   var prueba4=false;
+   var dolorDeCabeza=document.getElementsByName('DolorDeCabeza');
+
+   for(var i in dolorDeCabeza){
+     if(dolorDeCabeza[i].checked){
+       prueba4=true;
+     }
+   }
+
+   if(prueba4==false){
+     mensajeDeError+="<p>Responda si tuvo dolor de cabeza</p>";
+     error=false;
+   }
+
+   var prueba5=false;
+   var tos=document.getElementsByName('tos');
+
+   for(var i in tos){
+     if(tos[i].checked){
+       prueba5=true;
+     }
+   }
+
+   if(prueba5==false){
+     mensajeDeError+="<p>Responda si ha tenido tos</p>";
+     error=false;
+   }
+
+   var prueba6=false;
+   var dolorDeGarganta=document.getElementsByName('dolorDeGarganta');
+
+   for(var i in dolorDeGarganta){
+     if(dolorDeGarganta[i].checked){
+       prueba6=true;
+     }
+   }
+
+   if(prueba6==false){
+     mensajeDeError+="<p>Responsa si ha tenido dolor de garganta</p>";
+     error=false;
+   }
+
+   var prueba7=false;
+   var respirar=document.getElementsByName('respirar');
+
+   for(var i in respirar){
+     if(respirar[i].checked){
+       prueba7=true;
+     }
+   }
+
+   if(prueba7==false){
+     mensajeDeError+="<p>Responda si ha tenido dificultad para respirar</p>";
+   }
+
+
+
+  divContenedor.innerHTML=mensajeDeError;
+  divContenedor.style.color="red";
+  return error;
+}
